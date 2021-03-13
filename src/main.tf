5b23072456d4fe6adeb44f3befaf5e4781a4ca66
Eryckson Magno <eryckson@albedo.team>
2021-03-13 18:35:13 +0000

    Merged PR 167: fix terraform

--- src/main.tf
+++ src/main.tf
@@ -4,6 +4,14 @@ terraform {
       source  = "hashicorp/kubernetes"
       version = ">= 2.0.0"
     }
+    digitalocean = {
+      source  = "digitalocean/digitalocean"
+      version = "2.5.1"
+    }
+  }
+  backend "kubernetes" {
+    secret_suffix    = "communications-api"
+    load_config_file = true
   }
 }
 
@@ -11,59 +19,56 @@ provider "kubernetes" {
   config_path = "~/.kube/config"
 }
 
-resource "kubernetes_namespace" "communications" {
-  metadata {
-    name = var.src_name
-  }
-}
-
 resource "kubernetes_secret" "communications" {
   metadata {
-    name      = var.secret_name
-    namespace = kubernetes_namespace.communications.metadata.0.name
+    name      = var.project_secrets_name
+    namespace = var.namespace
   }
   data = {
-    Broker_Host = var.broker_connection_string
+    Broker_Host = var.settings_broker_connection_string
   }
 }
 
 resource "kubernetes_deployment" "communications" {
   metadata {
-    name      = var.src_name
-    namespace = kubernetes_namespace.communications.metadata.0.name
+    name      = var.project_name
+    namespace = var.namespace
     labels = {
-      app = var.deployment_label
+      app = var.project_label
     }
   }
 
   spec {
-    replicas = var.replicas_count
+    replicas = var.project_replicas_count
     selector {
       match_labels = {
-        app = var.src_name
+        app = var.project_name
       }
     }
     template {
       metadata {
         labels = {
-          app = var.src_name
+          app = var.project_name
         }
       }
       spec {
+        image_pull_secrets {
+          name = "${var.namespace}-do-registry"
+        }
         container {
-          image             = "${var.src_name}:latest"
-          name              = "${var.src_name}-container"
-          image_pull_policy = "IfNotPresent"
-          resources {
-            limits = {
-              cpu    = "0.5"
-              memory = "512Mi"
-            }
-            requests = {
-              cpu    = "250m"
-              memory = "50Mi"
-            }
-          }
+          image             = "${var.do_registry_name}/${var.project_name}:${var.project_image_tag}"
+          name              = "${var.project_name}-container"
+          image_pull_policy = "Always"
+          //          resources {
+          //            limits = {
+          //              cpu    = "0.1"
+          //              memory = "90Mi"
+          //            }
+          //            requests = {
+          //              cpu    = "0.1"
+          //              memory = "50Mi"
+          //            }
+          //          }
           port {
             container_port = 80
             protocol       = "TCP"
@@ -74,7 +79,7 @@ resource "kubernetes_deployment" "communications" {
           }
           env_from {
             secret_ref {
-              name = var.secret_name
+              name = var.project_secrets_name
             }
           }
         }
@@ -85,50 +90,28 @@ resource "kubernetes_deployment" "communications" {
 
 resource "kubernetes_service" "communications" {
   metadata {
-    name      = var.src_name
-    namespace = kubernetes_namespace.communications.metadata.0.name
+    name      = var.project_name
+    namespace = var.namespace
     labels = {
-      app = var.src_name
+      app = var.project_name
     }
   }
   spec {
-    type = "LoadBalancer"
+    type = "ClusterIP"
     port {
-      port        = var.service_port
-      target_port = "80"
+      name        = "http"
+      port        = 5300
+      target_port = 80
+      protocol    = "TCP"
+    }
+    port {
+      name        = "https"
+      port        = 5301
+      target_port = 443
       protocol    = "TCP"
     }
     selector = {
       app = kubernetes_deployment.communications.spec.0.template.0.metadata.0.labels.app
-    }
-  }
-}
-
-resource "kubernetes_ingress" "communications" {
-  metadata {
-    name      = var.src_name
-    namespace = kubernetes_namespace.communications.metadata.0.name
-    labels = {
-      app = var.src_name
-    }
-  }
-  spec {
-    backend {
-      service_name = var.src_name
-      service_port = var.service_port
-    }
-    rule {
-      host = "${var.environment-prefix}${var.subdomain}.${var.host}"
-      http {
-
-        path {
-          path     = "/"
-          backend {
-            service_name = var.src_name
-            service_port = var.service_port
-          }
-        }
-      }
     }
   }
 }
